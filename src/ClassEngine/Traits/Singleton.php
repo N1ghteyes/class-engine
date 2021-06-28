3e0451a02144721fc729c9b211cc64069ce4aa06
Toby New <toby.new@newitsolutions.co.uk>
2021-06-28 14:58:08 +0100

    correct the return type - stop phpstorm complaining.

--- src/ClassEngine/Traits/Singleton.php
+++ src/ClassEngine/Traits/Singleton.php
@@ -25,7 +25,7 @@ trait Singleton {
      * To return new or existing Singleton instance of the class from which it is called.
      * As it sets to final it can't be overridden.
      *
-     * @return object Singleton instance of the class.
+     * @return self Singleton instance of the class.
      */
     final public static function getInstance() {
         /**
